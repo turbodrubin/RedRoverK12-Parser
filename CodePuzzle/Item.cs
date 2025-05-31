849e5b36058f529e318d41e6e1e20283b384d7ee
David Rubin <drubin@perkspot.com>
2025-05-31 18:15:08 -0500

    Working with commentary.

--- CodePuzzle/Item.cs
+++ CodePuzzle/Item.cs
@@ -1,12 +1,13 @@
 ﻿namespace DRSoft.RedRoverK12.CodePuzzle
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Text.RegularExpressions;
 
+    // wWe declare a specific delegate type instead of relying on `Action<Item, int>` because it gives us a singular - and specific - type definition to use with Item.
     delegate void Operation(Item item, int level);
 
+    // This could be a record type, but we don't really care about the value-equality semantics so we'll stick with readonly struct.
     readonly struct Item
     {
         public Item(string name)
@@ -19,11 +20,10 @@
             Children = children;
         }
 
-        public static void Parse(string input)
-        {
+        public string Name { get; }
+        public Item[] Children { get; }
 
-        }
-
+        // This could have been `Print()` and `Print(ordering)`, but we'll indulge in a bit of functional-programming separate the operation from the tree-walking.
         public void Apply(Operation action, int level = 0)
         {
             action(this, level);
@@ -38,9 +38,34 @@
                 child.Apply(action, orderby, level + 1);
         }
 
-        public string Name { get; }
-        public Item[] Children { get; }
+        // Composites lend themselves naturally to recursion, don't they?
+        //
+        // This implementation assumes that `input` represents a single item (possibly with children).
+        // Hence the use of Match() on `input`, and Matches() when parsing out the children.
+        public static Item Parse(string input)
+        {
+            var match = _splitter.Match(input);
+            if (!match.Success)
+                // If we cared about performance, we could build some so-called "throw helpers". But we don't, so we won't.
+                throw new ArgumentException("input could not be parsed", nameof(input), new FormatException("input does not conform to expected format"));
 
+            var name = match.Groups["name"].Value;
+
+            var children = match.Groups["children"].Value;
+            if (children.Length > 0)
+            {
+                // `children` starts like "(child1, child2, ...)", so we trim the parenthesis before recursively applying the regex
+                var childMatches = _splitter.Matches(children[1..^1]);
+                var childItems = childMatches.Select(child => Parse(child.Value)).ToArray();
+                return new Item(name, childItems);
+            }
+            else
+                return new Item(name);
+        }
+
+        // Regexs can get nasty. The idea with this one is to look for strings like:
+        // (name1, name2, name3(child1, child2(grandchild1, grandchild2), child3), name4, ...)
+        // and capture the names and the entire child set for recursive parsing.
         private static readonly Regex _splitter = new(@"(?<name>\w*)(?<children>\(.*\))?\,?\s*");
     }
 }
--- CodePuzzle/Program.cs
+++ CodePuzzle/Program.cs
@@ -6,33 +6,25 @@
     {
         public static void Main(string[] args)
         {
-            var root = new Item("", [
-                new Item("id"),
-                new Item("name"),
-                new Item("email"),
-                new Item("type", [
-                    new Item("id"),
-                    new Item("name"),
-                    new Item("customFields", [
-                        new Item("c1"),
-                        new Item("c2"),
-                        new Item("c3")
-                    ]),
-                ]),
-                new Item("externalId")
-            ]);
+            // this could just as easily be `args[0]`
+            string input = "(id, name, email, type(id, name, customFields(c1, c2, c3)), externalId)";
 
+            var root = Item.Parse(input);
+
+            // given the input, we expect `root` not to have a name. PrintItems() accounts for empty names,
+            // but we don't want the extra indentation, so we start "back" one level.
             root.Apply(PrintItems, -1);
-            Console.WriteLine("=========================");
-            root.Apply(PrintItems, item => item.Name, -1);
 
-            Console.ReadKey();
+            Console.WriteLine("=========================");
+
+            root.Apply(PrintItems, item => item.Name, -1);
         }
 
         private static void PrintItems(Item item, int numIndents)
         {
             if (!String.IsNullOrWhiteSpace(item.Name))
             {
+                // this string constructor can only take a single space (' ') character, but we want an indent to be two spaces
                 string indents = new(' ', numIndents * 2);
                 Console.WriteLine($"{indents}- {item.Name}");
             }
