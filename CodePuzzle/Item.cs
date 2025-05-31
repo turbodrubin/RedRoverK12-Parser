68e486b80569768b02cf1954efde4c4348108a32
David Rubin <drubin@perkspot.com>
2025-05-31 17:23:17 -0500

    Printing works

---
+++ CodePuzzle/Item.cs
@@ -0,0 +1,46 @@
+﻿namespace DRSoft.RedRoverK12.CodePuzzle
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    delegate void Operation(Item item, int level);
+
+    readonly struct Item
+    {
+        public Item(string name)
+            : this(name, [])
+        { }
+
+        public Item(string name, Item[] children)
+        {
+            Name = name;
+            Children = children;
+        }
+
+        public static void Parse(string input)
+        {
+
+        }
+
+        public void Apply(Operation action, int level = 0)
+        {
+            action(this, level);
+            foreach (var child in Children)
+                child.Apply(action, level + 1);
+        }
+
+        public void Apply<TKey>(Operation action, Func<Item, TKey> orderby, int level = 0)
+        {
+            action(this, level);
+            foreach (var child in Children.OrderBy(orderby))
+                child.Apply(action, orderby, level + 1);
+        }
+
+        public string Name { get; }
+        public Item[] Children { get; }
+
+        private static readonly Regex _splitter = new(@"(?<name>\w*)(?<children>\(.*\))?\,?\s*");
+    }
+}
---
+++ CodePuzzle/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace DRSoft.RedRoverK12.CodePuzzle
+{
+    using System;
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var root = new Item("", [
+                new Item("id"),
+                new Item("name"),
+                new Item("email"),
+                new Item("type", [
+                    new Item("id"),
+                    new Item("name"),
+                    new Item("customFields", [
+                        new Item("c1"),
+                        new Item("c2"),
+                        new Item("c3")
+                    ]),
+                ]),
+                new Item("externalId")
+            ]);
+
+            root.Apply(PrintItems, -1);
+            Console.WriteLine("=========================");
+            root.Apply(PrintItems, item => item.Name, -1);
+
+            Console.ReadKey();
+        }
+
+        private static void PrintItems(Item item, int numIndents)
+        {
+            if (!String.IsNullOrWhiteSpace(item.Name))
+            {
+                string indents = new(' ', numIndents * 2);
+                Console.WriteLine($"{indents}- {item.Name}");
+            }
+        }
+    }
+}
